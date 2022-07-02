e69d29efaea31e1ff65cf32ddedf20cdee9944a8
Martin Samuel <87440756+martinsamuel10@users.noreply.github.com>
2022-07-02 15:21:13 +0700

    Add files via upload

---
+++ Topik4/GaleriHewan/app/src/main/java/org/d3if4004/galerihewan/Hewan.kt
@@ -0,0 +1,7 @@
+package org.d3rpla4404.galerihewan
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageResId: Int
+)
---
+++ Topik4/GaleriHewan/app/src/main/java/org/d3if4004/galerihewan/MainActivity.kt
@@ -0,0 +1,39 @@
+package org.d3rpla4404.galerihewan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if4077.galerihewan.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricoenis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi)
+        )
+    }
+}
